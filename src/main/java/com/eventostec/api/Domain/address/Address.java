f8ef5b77e73d0b3671d07e4321bc329ea04665cf
mariana.guirado <mariana.guirado@sesisenaisp.org.br>
2026-02-28 00:47:17 -0300

    feat: criando entidades

---
+++ src/main/java/com/eventostec/api/Domain/address/Address.java
@@ -0,0 +1,30 @@
+package com.eventostec.api.Domain.address;
+
+import com.eventostec.api.Domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.catalina.User;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ src/main/java/com/eventostec/api/Domain/event/Event.java
@@ -0,0 +1,32 @@
+package com.eventostec.api.Domain.event;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "event")
+@Entity
+@Getter
+@Setter
+//para construtor que receba todos ou nenhum argumentos dessa classe
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+
+    @Id
+    //significa que eh gerado automaticamente pela table
+    @GeneratedValue
+    private UUID id;
+    private String title;
+    private String description;
+    private String imgUrl;
+    private String eventUrl;
+    private Boolean remote;
+    private Date date;
+
+}
